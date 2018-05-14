a431acd046bd4d561c914be7cf6a2186f2b5fc51
Pavel Kurilov <pkurilov@spaceotechnologies.ru>
2018-05-15 02:23:14 +0700

    Initial Commit

---
+++ TestProjectSpaceoByKurilov/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProjectSpaceoByKurilov
+//
+//  Created by Pavel Kurilov on 15.05.2018.
+//  Copyright © 2018 Pavel Kurilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
